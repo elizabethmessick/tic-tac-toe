3f3e5bcb509987a62f96f2186d84f9f1b329971b
Elizabeth Messick <Elizabeth@MacBook-Pro-2.local>
2018-09-16 13:54:16 -0700

    changed html css and js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,167 @@
-/*----- constants -----*/
+var resetBtn;
+
+const container = document.querySelector('.container');
+
+// -----  PERSON FACTORY  -----
+const personFactory = (name, marker, playerNumber) => {
+  const displayPerson = () => {
+    document.querySelector(`#player${playerNumber}`).innerText = `${name}, ${marker}`
+  };
+
+  displayPerson();
+  
+  return { name, marker, displayPerson };
+};
 
 
-/*----- app's state (variables) -----*/
-var x;
-var o;
+
+// -----  GAME FACTORY  -----
+const gameFactory = (player1, player2) => {
+  document.querySelector('.gameInformation').classList.remove('hidden');
+  document.querySelector('.getPlayerInfo').classList.add('hidden')
+
+  let currentPlayer = player1;
+  let gameArray = Array(9).fill('');
+
+  // Checks if all items in array are the samef
+  Array.prototype.allSameValues = function() {
+    if (this[0] === "") { return false; }
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] !== this[0]) { return false; }
+    } 
+    return true;
+  }
+
+  const validChoice = (target) => {
+    const targetIndex = target.id.slice(-1);
+    return (gameArray[targetIndex] === "") ? true : false;
+  }
+
+  const playerChoose = (currentPlayer, target) => {
+    const targetIndex = target.id.slice(-1);
+    const playerMarker = currentPlayer.marker;
+    if (gameArray[targetIndex] === "") {
+      gameArray[targetIndex] = playerMarker;
+    }
+  }
+
+  const renderBoard = () => {
+    const boxes = document.querySelectorAll('.box')
+    boxes.forEach(function(box, index) {
+      box.innerHTML = `<p>${gameArray[index]}</p>`
+    })
+  };
+
+  const switchPlayer = (currentPlayer) => {
+    return (currentPlayer === player1) ? player2 : player1;
+  };
+
+  const checkForTie = (gameArray) => {
+    let tieGame = true;
+    gameArray.forEach(function(position) {
+      if (position === "") { tieGame = false; }
+    })
+    return tieGame;
+  }
+
+  const checkForWin = (gameArray) => {
+    let win = false;
+
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    winningCombos.forEach(arr => {
+      const a = gameArray[arr[0]];
+      const b = gameArray[arr[1]];
+      const c = gameArray[arr[2]];
+      if ([a, b, c].allSameValues()) {
+        win = true;
+        return;
+      };
+    });
+
+    return win;
+  }
+
+  const gameOver = player => {
+    // If not passed ARG, tie game is displayed
+    const messageAnchor = document.querySelector(".message")
+    const gameInformation = document.querySelector('.gameInformation')
+
+    const changeScreen = (displayMessage) => {
+      container.style.display = 'none';
+      gameInformation.style.display = 'none';
+      messageAnchor.innerHTML = `<h1>${displayMessage}</h1>`
+    }
+
+    const tieGame = () => { changeScreen("Neither of you are very good! Haha!"); }
+
+    const winner = () => { changeScreen(`${player.name} wins!`); }
+    
+    player === undefined ? tieGame() : winner()
+  }
+
+  container.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.className === "box") {
+      let valid = validChoice(target)
+      if (valid) {
+        playerChoose(currentPlayer, target);
+        renderBoard();
+        const tie = checkForTie(gameArray);
+        const win = checkForWin(gameArray, currentPlayer);
+        if (tie) {
+          gameOver();
+        } else if (win) {
+          gameOver(currentPlayer);
+        } else {
+          currentPlayer = switchPlayer(currentPlayer);
+        }
+      }
+    };
+  });
+};
 
 
-/*----- cached element references -----*/
-var resetButton;
-var gameBoard;
 
-
-/*----- event listeners -----*/
-function playerOne(event) {
-
+// ----- RESET GAME -----
+function reset() {
+  //re-initilize variables 
+  gameOver = false; 
+ 
+  //show board empty 
 }
 
-function reset(event) {
 
-
-}
-/*----- functions -----*/
-
-function initGame() {
-
-}
-
-function endGame(win) {
-
-}
-
-
+// ----- INIT GAME -----
+document.querySelector('#startGame').addEventListener('click', function() {
+  const player1Name = document.querySelector('#player1Name').value
+  const player2Name = document.querySelector('#player2Name').value
+  let player1 = personFactory(player1Name, "kisses", 1);
+  let player2 = personFactory(player2Name, "hugs", 2);
+  gameFactory(player1, player2);
+})
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM loaded");
+  console.log("DOM loaded");
 
-});
+  resetBtn = document.getElementById('resetGame');
+
+  resetBtn.addEventListener('click', reset);
+
+
+
+})
+
 
 
 //initilize game 

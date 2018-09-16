5d493abd4d756654eff8ed02eff109c129f1009b
Elizabeth Messick <Elizabeth@MacBook-Pro-2.local>
2018-09-15 18:44:23 -0700

    created html css files

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+var x;
+var o;
+
+
+/*----- cached element references -----*/
+var resetButton;
+var gameBoard;
+
+
+/*----- event listeners -----*/
+function playerOne(event) {
+
+}
+
+function reset(event) {
+
+
+}
+/*----- functions -----*/
+
+function initGame() {
+
+}
+
+function endGame(win) {
+
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM loaded");
+
+});
+
+
+//initilize game 
+//2 players click boxes 
+//once a box is clicked it can't be clicked again 
